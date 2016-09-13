98d8f03dc004d71c285cce3a0c5f67c23e7f81f7
insung <imissyoubrad@gmail.com>
2016-09-13 10:22:08 +0900

    Get Backload ClassicASPXDemo.aspx
    the original file is Other/ASPNet/ClassicASPXDemo.aspx

--- Backload.Net4/Default.aspx.designer.cs
+++ Backload.Net4/Default.aspx.designer.cs
@@ -11,14 +11,5 @@ namespace Backload.Net4 {
     
     
     public partial class Default {
-        
-        /// <summary>
-        /// form1 컨트롤입니다.
-        /// </summary>
-        /// <remarks>
-        /// 자동 생성 필드입니다.
-        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하십시오.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
     }
 }
