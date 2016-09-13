5109d20251e0aef7fa4f6182b1dc1a0e49b7a8f5
insung <imissyoubrad@gmail.com>
2016-09-13 10:00:44 +0900

    filehandler without async await keyword

---
+++ Backload.Net4/FileUploadHandler.ashx.cs
@@ -0,0 +1,49 @@
+﻿using Backload.Contracts.Context;
+using Backload.Contracts.FileHandler;
+using Backload.Helper;
+using System;
+using System.Web;
+
+namespace Backload.Net4
+{
+  /// <summary>
+  /// FileUploadHandler의 요약 설명입니다.
+  /// </summary>
+  public class FileUploadHandler : IHttpHandler
+  {
+
+    public void ProcessRequest(HttpContext context)
+    {
+      try
+      {
+        // Wrap the request into a HttpRequestBase type
+        HttpRequestBase request = new HttpRequestWrapper(context.Request);
+        // Create and initialize the handler
+        IFileHandler handler = Backload.FileHandler.Create();
+
+        // Init Backload execution environment and execute the request
+        handler.Init(request);
+
+        // Call the execution pipeline and get the result
+        IBackloadResult result = handler.Execute().Result;
+
+        // Write result to the response and flush
+        ResultCreator.Write(context.Response, result);
+        context.Response.Flush();
+      }
+      catch (Exception ex)
+      {
+        context.Response.StatusCode = 500;
+        context.Response.StatusDescription = ex.Message;
+      }
+    }
+
+    public bool IsReusable
+    {
+      get
+      {
+        return false;
+      }
+    }
+  }
+}
